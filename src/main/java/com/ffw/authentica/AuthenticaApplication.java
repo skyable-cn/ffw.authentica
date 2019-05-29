ada566e134c64597eb0d21619648de027347a126
mahonggang <1584157651@qq.com>
2019-05-29 23:06:30 +0800

    init

---
+++ src/main/java/com/ffw/authentica/AuthenticaApplication.java
@@ -0,0 +1,13 @@
+package com.ffw.authentica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticaApplication.class, args);
+	}
+
+}
